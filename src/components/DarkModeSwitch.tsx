8f9ff2220f60520f96a8859e37803e6d98a920c9
qazal <ghazal.nzhrzr@gmail.com>
2022-05-29 10:56:50 +0400

    fix(theme): fix color mode flash

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,14 +1,19 @@
 import { useColorMode, IconButton } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import useSound from "use-sound";
 import { useTheme } from "next-themes";
 
 export const DarkModeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
   const isDark = theme === "dark";
   const [playSwitchSound] = useSound("/sounds/lightSwitch.mp3", {
     volume: 0.25,
   });
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
 
   function handleChangeMood() {
     playSwitchSound();
